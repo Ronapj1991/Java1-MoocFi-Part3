6a41ea10a86495eb7983741a98b2b63c713f207b
Ronapj1991 <51872051+Ronapj1991@users.noreply.github.com>
2021-11-01 14:21:31 -0400

    Create Part03_23.PrintThrice.java

---
+++ Part03_23.PrintThrice.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+ 
+
+public class PrintThrice {
+
+ 
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+ 
+
+        // Write your program here
+
+        String user = scanner.nextLine();
+
+        System.out.println(user + user + user);
+
+    }
+
+}
