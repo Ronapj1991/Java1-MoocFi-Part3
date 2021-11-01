46110f49bf004037014b56fca9fc8f5e326138c9
Ronapj1991 <51872051+Ronapj1991@users.noreply.github.com>
2021-11-01 14:15:59 -0400

    Create Part03_16.Sum.java

---
+++ Part03_16.Sum.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+ 
+
+ 
+
+public class Sum {
+
+ 
+
+    public static void main(String[] args) {
+
+        // Try your method here
+
+    }
+
+ 
+
+    public static int sum(ArrayList<Integer> numbers) {
+
+		int total = 0;
+
+		
+
+		for (int eachNumTotal:numbers) {
+
+			total += eachNumTotal;
+
+		}
+
+		
+
+		return total;
+
+	}
+
+}
